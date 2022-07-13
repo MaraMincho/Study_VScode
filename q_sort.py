00f35e0647a563fe9a1f219a544cf24ab7a376e5
maramincho <seemaster300@naver.com>
2022-07-14 00:24:02 +0900

    saklfasjdkf

--- q_sort.py
+++ q_sort.py
@@ -1,36 +1,26 @@
-def quick_sort(arr):
-    def sort(low, high):
-        if high <= low:
-            return
+import csv
+import requests
+from bs4 import BeautifulSoup
 
-        mid = partition(low, high)
-        sort(low, mid - 1)
-        sort(mid, high)
+filename ="시가총액.csv"
+f = open(filename, "w", encoding="utf-8-sig" ,newline="")
+writer = csv.writer(f)
 
-    def partition(low, high):
-        pivot = arr[(low + high) // 2]
-        while low <= high:
-            while arr[low] < pivot:
-                low += 1
-            while arr[high] > pivot:
-                high -= 1
-            if low <= high:
-                arr[low], arr[high] = arr[high], arr[low]
-                low, high = low + 1, high - 1
-        return low
-
-    return sort(0, len(arr) - 1)
-  
-  
-import random
-arr = []
-for i in range(10) :
-  i = random.randint(1, 100)
-  arr.append(i)
-
-print("before", arr)
+temp = "N	종목명	현재가	전일비	등락률	액면가	시가총액	상장주식수	외국인비율	거래량	PER	ROE	토론실"
+temp = temp.split("\t")
+writer.writerow(temp)
 
 
-quick_sort(arr)
-
-print("after", arr)
+url = "https://finance.naver.com/sise/sise_market_sum.nhn?sosok=0&page="
+for page in range(1, 2) :
+    res = requests.get(url + str(page))
+    res.raise_for_status()
+    soup = BeautifulSoup(res.text, "html.parser")
+    
+    data_rows = soup.find("table", attrs={"class" : "type_2"}).find("tbody").find_all("tr")
+    for row in data_rows :
+        columns = row.find_all("td")
+        if len(columns) <= 1 :
+            continue
+        data = [column.get_text().strip() for column in columns]
+        writer.writerow(data)
