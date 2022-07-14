a70ff9dd401ad86607ad3ab4dc1434dc4faf3bf9
maramincho <seemaster300@naver.com>
2022-07-14 22:14:19 +0900

    gihig

--- web_scrapping/12_selenum.py
+++ web_scrapping/12_selenum.py
@@ -1,4 +1,18 @@
 from selenium import webdriver
 
 browser = webdriver.Chrome("./chromedriver")
+
+#네이버로 이ㅇ
 browser.get("https://www.naver.com/")
+
+
+element = browser.find_element_by_class_name("link_login")
+element.click()
+
+# browser.find_element_by_id("id").send_keys("naver_id")
+
+# browser.find_element_by_id("pw").send_keys("naver_id")
+
+# browser.find_elemnt_by_id("log.login").click()s
+
+
---
+++ web_scrapping/13_naver_airplan.py
@@ -0,0 +1,7 @@
+class Deque : 
+  rear = 0
+  front = 0
+  MAX_SIZE = 100
+  deq = list()
+  def __init__ (self) :
+    
